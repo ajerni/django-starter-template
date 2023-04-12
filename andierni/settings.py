906e37fd1a6a25c232175a35e08125aed903f2e0
ajerni <ajerni@gmail.com>
2023-04-12 15:35:46 +0000

    static root set

--- andierni/settings.py
+++ andierni/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'andierni/static') ]
 
 
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
