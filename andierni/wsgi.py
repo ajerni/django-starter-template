b2283f1a7e8ae68f0655af0c76f9437274659a87
ajerni <ajerni@gmail.com>
2023-04-12 09:27:35 +0000

    wsgi.py set to app

--- andierni/wsgi.py
+++ andierni/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hello_world.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "andierni.settings")
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
+app = get_wsgi_application()
