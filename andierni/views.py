a399a75bc5c385996f5156ca3f8b8464d5526d71
ajerni <ajerni@gmail.com>
2023-04-12 16:12:34 +0000

    Django Starter Template v1

--- andierni/views.py
+++ andierni/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html', {})
 
-
 def test(request):
     import requests
     response = requests.get('https://djangoerni.pythonanywhere.com/courses/')
