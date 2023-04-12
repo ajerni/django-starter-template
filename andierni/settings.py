666e60255310df180957cff80fa7566555500b64
ajerni <ajerni@gmail.com>
2023-04-12 16:03:59 +0000

    staticicici

--- andierni/settings.py
+++ andierni/settings.py
@@ -123,15 +123,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-#STATICFILES_DIRS = [BASE_DIR / "andierni" / "static"]
-
-STATIC_URL = "staticfiles/static/"
+STATIC_URL = "static/"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'andierni/static') ]
 
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
